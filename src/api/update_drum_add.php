a9176137ed684a699fe2e54ed09a8cd2d1f1f5c9
Shubham Mishra <codermishra53@gmail.com>
2026-02-10 14:05:49 +0530

    Add product_type to update_drum_add allowed fields

--- src/api/update_drum_add.php
+++ src/api/update_drum_add.php
@@ -106,7 +106,7 @@ $update_fields = [];
 $updates = [];
 
 // List of allowed fields to update
-$allowed_fields = ['title', 'description', 'price_per_month', 'security_deposit', 'city', 'latitude', 'longitude', 'brand'];
+$allowed_fields = ['title', 'description', 'price_per_month', 'security_deposit', 'city', 'latitude', 'longitude', 'brand', 'product_type'];
 
 foreach($allowed_fields as $field) {
     if(isset($input[$field])) {
