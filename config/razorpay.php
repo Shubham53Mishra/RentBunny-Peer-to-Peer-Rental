4a20912a5f454ae4debd77ef08730e1c59f82902
Shubham Mishra <codermishra53@gmail.com>
2026-02-13 23:33:07 +0530

    Update changes

--- config/razorpay.php
+++ config/razorpay.php
@@ -3,13 +3,13 @@
 
 // Production Keys
 define('RAZORPAY_KEY_ID', 'rzp_live_wwpGzdI2GDWS29');
-define('RAZORPAY_KEY_SECRET', ''); // Add your secret key here
+define('RAZORPAY_KEY_SECRET', 'II29D8pGMGLBYWGZn8qgo2Pq');
 
 // Razorpay API Endpoints
 define('RAZORPAY_API_URL', 'https://api.razorpay.com/v1');
 
 // Callback URLs
-define('RAZORPAY_CALLBACK_URL', 'http://' . $_SERVER['HTTP_HOST'] . '/Rent/src/api/payments/verify_payment.php');
-define('RAZORPAY_WEBHOOK_URL', 'http://' . $_SERVER['HTTP_HOST'] . '/Rent/src/api/payments/webhook.php');
+define('RAZORPAY_CALLBACK_URL', 'http://' . $_SERVER['HTTP_HOST'] . '/api/payments/verify_payment.php');
+define('RAZORPAY_WEBHOOK_URL', 'http://' . $_SERVER['HTTP_HOST'] . '/api/payments/webhook.php');
 
 ?>
